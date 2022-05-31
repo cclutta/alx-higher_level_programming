2356a9149e6e38863f6b5876465ea9db95c64731
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-01 00:31:07 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
@@ -12,4 +11,4 @@ def fizzbuzz():
         else:
             print("{} ".format(i), end='')
 
-fizzbuzz()
+
