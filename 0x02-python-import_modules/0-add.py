6aa828327dd70a0a8f69fd4100333767c6a298a3
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-02 13:18:45 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,6 @@ from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
    
-if __name__ = "__main__":
-    main()
