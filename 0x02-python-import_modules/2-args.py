8ac60d17123d69595a3bbb09e22ac898b3d202e7
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-02 23:47:19 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ import sys
 def main():
     argc = len(sys.argv) - 1
     if argc == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{:d} arguments:", format(argc))
