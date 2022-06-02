f7768024784f1dd6a75b406448762f32fadf452b
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-02 23:54:51 +0300

    Create 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argc = len(sys.argv)
+    for i in range(1, argc):
+        c += int(sys.argv[i])
+    print(c)
+      
+
+if __name__ == "__main__":
+    main()
