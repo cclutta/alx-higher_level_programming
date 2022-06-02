7706af3e9cac54d52ba9457adf0259f408675631
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-03 00:15:19 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,8 @@ import hidden_4
 
 def main():
     file = dir(hidden_4)
-    l = len(file)
-    for i in range(0, l):
+    l1 = len(file)
+    for i in range(0, l1):
         if file[i][0:2] != "__":
             print(file[i])
 
