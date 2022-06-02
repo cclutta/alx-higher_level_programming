fa8cceb8f4fc14fc01eea8fac6a969a65a481589
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-03 00:14:36 +0300

    Create 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    file = dir(hidden_4)
+    l = len(file)
+    for i in range(0, l):
+        if file[i][0:2] != "__":
+            print(file[i])
+
+
+if __name__ == "__main__":
+    main()
