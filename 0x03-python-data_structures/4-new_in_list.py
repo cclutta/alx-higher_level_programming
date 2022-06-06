d26b4c85870ec1eb5134352cdcf049de3f897b2e
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-06 18:22:23 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < len(new_list) and idx >= 0:
         new_list[idx] = element
