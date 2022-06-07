e0805102eb7ca2ae71ab226c3f60ae7174bd2149
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-07 18:20:29 +0300

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for r in matrix:
+        for c in range(len(r)):
+            print("{:d}",format(r[c]), end= ' 'if c < len(r) - 1 else '')
+        print()
