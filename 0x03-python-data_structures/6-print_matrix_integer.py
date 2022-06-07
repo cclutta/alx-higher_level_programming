99abb46328227f0cd6b8ca53b036bba332728331
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-07 18:22:04 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,6 @@
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
         for c in range(len(r)):
-            print("{:d}",format(r[c]), end= ' 'if c < len(r) - 1 else '')
+            print("{:d}",format(r[c]), 
+                  end= ' ' if c < len(r) - 1 else '')
         print()
