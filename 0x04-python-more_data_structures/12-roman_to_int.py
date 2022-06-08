ee31d46cd0428810d473f9e844c66395d6f192df
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-08 22:03:47 +0300

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    num = 0
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) -1, -1, -1):
+            if roman[roman_string[c]] >= i:
+                num += roman[roman_string[c]]
+            else:
+                num -= roman[roman_string[c]]
+            i = roman[roman_string[c]]
+    return num
