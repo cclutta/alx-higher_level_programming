dbe1440852ba25ad515d477237b1df24fccc2b95
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-08 17:14:21 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    for k, v in sorted(a_dictionary.items()):
-        if k == key:
-            v = value
-        else:
-            a_dictionary['key'] = value
+    a_dictionary[key] = value
+    return a_dictionary
