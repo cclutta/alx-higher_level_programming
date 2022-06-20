c63ec5dc0b98fc5729f9998fdec6fdf57fec3ab0
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-20 12:12:08 +0300

    Create 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        d = a / b
+    except ZeroDivisionError:
+        d = None
+    finally:
+        print("Inside result: {:d}".format(d))
+    return d
+        
