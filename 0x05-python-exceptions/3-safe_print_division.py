16ce6408d842983b87454b989bc5290601d68b84
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-20 12:13:11 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         d = None
     finally:
-        print("Inside result: {:d}".format(d))
+        print("Inside result: {}".format(d))
     return d
         
