923a45cadad54fc24e6f2e74e053e0d0e89be065
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-20 12:21:36 +0300

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     list = []
     try:
-        for i in ranger(list_length):
+        for i in range(list_length):
             list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             list.append(0)
