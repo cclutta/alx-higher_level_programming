e8b2fa9db8acdc86c87b158fca8feeef48727e99
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-21 15:55:49 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module square
+"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """ Sets private attribute size for square
+        """
+        self.__size = size
+    
