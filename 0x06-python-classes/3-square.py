ca7d245b2e11fe168904724986ce2bed39508cec
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-21 16:07:59 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module square
+"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """ Sets private attribute size for square
+        Args:
+            size: size of side of square
+        Raises:
+            TypeError: when not int
+            ValueError: when less than 0
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+ 
+
+    def area(self):
+        """ Returns square area. """
+        return self.__size ** 2
