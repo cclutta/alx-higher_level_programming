084ba2ff515d3aac76ba794845e475eb40a4eb12
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-27 10:31:14 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,16 +17,16 @@ def add_integer(a, b=98):
     if type(a) in [int, float]:
         try:
             a = int(a)
-        except:
-            raise TypeError("a must be an integer")
+        except TypeError:
+            print("a must be an integer")
     else:
         raise TypeError("a must be an integer")
         
     if type(b) in [int, float]:
         try:
             b = int(b)
-        except:
-            raise TypeError("b must be an integer")
+        except TypeError:
+            print("b must be an integer")
     else:
         raise TypeError("b must be an integer")
     return a + b
