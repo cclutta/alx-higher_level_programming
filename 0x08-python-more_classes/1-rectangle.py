7b956f531599875569f42069dd339f4ca512dc08
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-28 14:11:15 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
             width: width of rect
             height: height of rect
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
