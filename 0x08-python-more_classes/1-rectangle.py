1c850e3d8f19e96f2241f3875eb540a235ae26a3
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-28 14:09:53 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if (type) value is int:
@@ -36,7 +36,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-  
+
     @height.setter
     def height(self, value):
         if (type) value is int:
