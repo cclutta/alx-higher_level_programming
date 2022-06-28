f98a05de37034139b6d1c0374463cbedc9a5ac4b
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-28 17:28:07 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""
-Module rectangle
-
-Class that defines rectange
-
+"""Rectangle module.
+This module contains a class that defines a rectangle.
 """
 
 
-class Rectangle:
-    """
-    Rectangle class
-    """
+class Rectangle():
+    """Defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """
-        Instatiation of private attributes
-
+        """Sets the necessary attributes for the Rectangle object.
         Args:
-            width: width of rect
-            height: height of rect
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ Returns rectangle width"""
+        """Get or set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if (type) value is int:
+        if type(value) is int:
             if value >= 0:
                 self.__width = value
             else:
@@ -40,14 +33,14 @@ class Rectangle:
 
     @property
     def height(self):
-        """ Returns rectangle height """
+        """Get or set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if (type) value is int:
+        if type(value) is int:
             if value >= 0:
-                self.height = value
+                self.__height = value
             else:
                 raise ValueError("height must be >= 0")
         else:
