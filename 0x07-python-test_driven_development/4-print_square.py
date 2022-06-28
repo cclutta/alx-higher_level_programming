92b691eefef709743312b6e8bc55a1a6cd71ab61
cclutta <52520502+cclutta@users.noreply.github.com>
2022-06-28 21:12:24 +0300

    Create 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+print_square module.
+
+one function, print_square().
+"""
+
+
+def print_square(size):
+    """
+    Print a square with the character #.
+    Args:
+        size (int): the size length of the square.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print(("#" * size + '\n') * size, end='')
