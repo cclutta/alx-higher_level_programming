3751624396324169ba5e69b88ed3ca13e121f3f8
cclutta <chelsea.lutta@gmail.com>
2022-07-04 21:43:35 +0300

    Update

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+base_geometry module
+
+Contains class base_geometry
+"""
+
+
+class BaseGeometry:
+    """Base Geometry class. """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+base_geometry module
+
+Contains class base_geometry
+"""
+
+
+class BaseGeometry:
+    """Base Geometry class. """
+    def area(self):
+        """ Function to get area. """
+        raise Exception("area() is not implemented")
