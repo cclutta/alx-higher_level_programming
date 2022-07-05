a56e280aa608f9bf7ad708863003fb0499d1ddeb
cclutta <52520502+cclutta@users.noreply.github.com>
2022-07-06 01:11:25 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,8 +6,8 @@ Contains student class
 
 
 def Student:
-    """ Student class """
-
+    """ Student class. """
+    
     def __init__(self, first_name, last_name, age):
         """ Sets attributes for student object """
  
