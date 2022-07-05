b07029ea992de2968639ee30b736e75a1cc81c49
cclutta <52520502+cclutta@users.noreply.github.com>
2022-07-06 00:51:23 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,4 @@ import json
 def load_from_json_file(filename):
     """ that creates an Object from a JSON file. """
     with open(filename, 'r') as f:
-        lines = f.readlines()
-        return json.loads(lines)
+        return json.load(f)
