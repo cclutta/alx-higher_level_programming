5a2fe0e641e8d42d3ef7c85d903ce8dc9f390027
cclutta <52520502+cclutta@users.noreply.github.com>
2022-07-06 01:05:19 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,7 +10,7 @@ def Student:
 
     def __init__(self, first_name, last_name_ age):
         """ Sets attributes for student object """
-        
+ 
         Args:
             first_name: first name
             last_name: last name
@@ -23,4 +23,3 @@ def Student:
     def to_json(self):
         """ Returns dict desc of student """
         return self.__dict__
-        
