e1eec2ee433e5012f69094fa699495f846de2bf4
cclutta <chelsea.lutta@gmail.com>
2022-07-06 10:16:03 +0300

    Update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Add item script."""
+"""Add item script. """
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-lst = load_from_json_file("add_item.json")
+try:
+    lst = load_from_json_file("add_item.json")
+except EXception as e:
+    lst = []
 
 argc = len(sys.argv)
 
