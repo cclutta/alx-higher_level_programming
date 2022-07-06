fd7539859e7980ff83ad1efb541e3bf6f0b1547d
cclutta <chelsea.lutta@gmail.com>
2022-07-06 10:26:21 +0300

    Update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Student Module
-Contains student class
+"""Student module.
+Contains a Student class.
 """
 
 
-def Student:
-    """class Student that defines a student by. """
+class Student:
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize new student
-
+        """Sets attributes for the Student object.
         Args:
-            first_name: first name of student
-            last_name : last name of student
-            age: age of student
+            first_name: first name of the student.
+            last_name: last name of the student.
+            age: age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Returns dict desc of student """
+        """Retrieves a dictionary representation of a Student instance."""
         return self.__dict__
