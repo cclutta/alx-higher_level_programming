5df1678785de0ce25bc5fa661b06bd823b678ee7
cclutta <chelsea.lutta@gmail.com>
2022-07-12 18:32:31 +0300

    Update

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+"""Rectangle module.
+This module contains a class that defines a rectangle.
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Defines a rectangle."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the default attributes of the Base object.
+
+        Args:
+            width (int): width
+            height (int): height
+            x (int): horizontal padding
+            y (int): vertical padding
+            id (int): identifier of object
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        """Overrides __str__ method."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    @property
+    def width(self):
+        """Get or set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get or set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+            
+    @property
+    def x(self):
+        """Get or set the width of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if value is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+            
+    @property
+    def y(self):
+        """Get or set the width of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if value is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ Returns area. """
+        return self.__width * self.__height
+            
+    def display(self):
+       """Displays rectangle. """
+       for i in range(self.__height):
+           print('#' * self.__width)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
---
+++ 0x0C-python-almost_a_circle/models/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+width = 2
+height = 2
+
+for i in range(height):
+    print("#" * width)
