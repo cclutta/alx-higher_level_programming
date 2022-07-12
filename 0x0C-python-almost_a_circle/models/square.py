293db39a77cfeb9b9b72868d3705e5b58ecf30c8
cclutta <chelsea.lutta@gmail.com>
2022-07-12 22:23:13 +0300

    Update

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square module
+Defines the square class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from rectangle."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Used to initiate attributes
+
+        Args:
+            size: size of the square
+            x: x attrubute
+            y: y attribute
+            id: class Id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overrides __str__ method."""
+        return "[Square] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
