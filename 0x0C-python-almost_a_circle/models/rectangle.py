862b5e331213e2e240c6a058f4e67f075e48ff64
cclutta <chelsea.lutta@gmail.com>
2022-07-12 22:32:51 +0300

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,10 @@ class Rectangle(Base):
                     self.__init__(self.width, self.height, self.x, self.y)
                 else:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id, 'width': self.width, 'height': self.height,
+            'x': self.x, 'y': self.y
+        }
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,3 +50,9 @@ class Square(Rectangle):
                     self.__init__(self.size, self.x, self.y)
                 else:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y
+        }
