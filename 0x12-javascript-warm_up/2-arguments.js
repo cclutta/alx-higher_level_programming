654a3b3a942db8d60694ee5b52859d9b7d45086c
cclutta <chelsea.lutta@gmail.com>
2022-09-05 21:58:26 +0300

    Fix task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const l = process.argv.length;
 
 if (l < 3) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (l === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
