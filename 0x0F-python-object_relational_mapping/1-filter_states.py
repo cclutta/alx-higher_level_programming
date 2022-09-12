8740217bd259615679edee092b0a683a14e6ac9c
cclutta <chelsea.lutta@gmail.com>
2022-09-12 19:06:27 +0300

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Filter states module from database hbtn_0e_0_usa script
+
+Args:
+    mysql username
+    mysql password
+    mysql database name
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    db = MySQLdb.connect(user=mysql_u, passwd=mysql_p, db=mysql_db)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE \"N%\" ORDER BY id;")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
