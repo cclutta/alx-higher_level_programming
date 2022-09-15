a0a30004070bf4e638e53e96ce06bee0f0548112
cclutta <chelsea.lutta@gmail.com>
2022-09-15 12:13:55 +0300

    UPdate

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+
+Args:
+    mysql username
+    mysql password
+    mysql database name
+"""
+
+from model_state import Base, State
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm.session import Session
+
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+    mysql_h = "localhost"
+   
+    url = {'drivername': 'mysql+mysqldb', 'host': mysql_h,
+               'username': mysql_u, 'password': mysql_p, 'database': mysql_db}
+    eng = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    
+    session = Session(bind=engine)
+    
+    res = session.query(State).order_by(State.id)
+    
+    for i in res:
+        print("{}: {}".format(i.id, i.name))
