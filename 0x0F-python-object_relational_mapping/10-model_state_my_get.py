46bc54564e33d4403e23a32dcb1ccf396be6ddcf
cclutta <chelsea.lutta@gmail.com>
2022-09-15 12:42:23 +0300

    Update

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all State objects with name passed as arg
+
+Args:
+    mysql username
+    mysql password
+    mysql database name
+"""
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm.session import Session
+
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+    mysql_h = "localhost"
+    state_name = sys.argv[4]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': mysql_h,
+           'username': mysql_u, 'password': mysql_p, 'database': mysql_db}
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    res = session.query(State).filter(State.name == state_name).order_by(State.id)
+
+    if res.first():
+        print(res.first().id)
+    else:
+        print("Not found")
