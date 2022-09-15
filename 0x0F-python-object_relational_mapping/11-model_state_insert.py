f5d5ddd0d49068db5fdb0c2f3fd3cd6a167f8e28
cclutta <chelsea.lutta@gmail.com>
2022-09-15 12:48:41 +0300

    UPdate

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+adds new State object
+
+Args:
+    mysql username
+    mysql password
+    mysql database name
+"""
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm.session import Session
+
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+    mysql_h = "localhost"
+
+    url = {'drivername': 'mysql+mysqldb', 'host': mysql_h,
+           'username': mysql_u, 'password': mysql_p, 'database': mysql_db}
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    new = State(name="Louisiana")
+
+    session.add(new)
+    session.commit()
+
+    print(new.id)
