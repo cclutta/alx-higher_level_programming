bb01f4ee345f33df070ca76f50221af0bc716813
cclutta <52520502+cclutta@users.noreply.github.com>
2022-09-17 04:26:12 +0300

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     session = Session(bind=engine)
 
-    res = session.query(State).filter(State.name.like('%a%'))
+    res = session.query(State).filter(State.name.like("%a%"))
     res.delete(synchronize_session=False)
 
     session.commit()
