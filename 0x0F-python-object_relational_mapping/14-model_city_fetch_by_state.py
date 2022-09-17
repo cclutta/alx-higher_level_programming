94f29c900edf27f60f8262f560b6258d4b583d37
cclutta <52520502+cclutta@users.noreply.github.com>
2022-09-17 04:45:56 +0300

    Update 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that prints all `City` objects from the db
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mysql_u, 'password': mysql_p, 'database': mysql_db}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    q = session.query(City, State).filter(City.state_id == State.id)
+
+    for city, state in q:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
