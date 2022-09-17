63788508217a108b78d6abda01d6b9ddd14cba6d
cclutta <chelsea.lutta@gmail.com>
2022-09-17 05:36:46 +0300

    Update

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that prints all `City` objects from the db
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    mysql_u = sys.argv[1]
+    mysql_p = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
+           'username': mysql_u, 'password': mysql_p, 'database': mysql_db}
+
+    engine = create_engine(URL(**url), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(bind=engine)
+
+    new_state = State(name="California")
+    new_state.cities.append(City(name="San Francisco"))
+
+    session.add(new_state)
+    session.commit()
