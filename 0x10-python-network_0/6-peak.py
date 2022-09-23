c0c032ee3c21770d32076ed5ae0f930e8cd30872
cclutta <52520502+cclutta@users.noreply.github.com>
2022-09-23 20:10:36 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,12 +16,12 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if l_len == 2:
         return max(list_of_integers)
-      
-      mid = int(l_len / 2)
-      peak = list_of_integers[mid]
-      if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-          return peak
-      elif peak > list_of_integers[mid + 1]:
-          return find_peak(list_of_integers[:mid])
-      else:
-          return find_peak(list_of_integers[mid + 1:])
+
+    mid = int(l_len / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak > list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
