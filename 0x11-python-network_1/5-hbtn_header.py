e000443193947292a0252e8fb44578c036edf6f0
cclutta <chelsea.lutta@gmail.com>
2022-09-25 20:08:15 +0300

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" That fetches and displays the value of the variable
+    X-Request-Id in the response header
+    Args:
+        url: the url to fetch
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    content = requests.get(url)
+    print(content.headers['X-Request-Id'])
