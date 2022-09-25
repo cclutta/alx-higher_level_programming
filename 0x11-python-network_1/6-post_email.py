9d2651c67c78b4141a8138585b674b61d41df3f0
cclutta <chelsea.lutta@gmail.com>
2022-09-25 20:20:24 +0300

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" that takes in a URL and an email, sends a POST request to the passed URL
+    Args:
+        url: the url to fetch
+        email: email to be passed
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    values = {"email": email}
+    res = requests.post(url, data=values)
+    print(res.text)
