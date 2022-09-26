8e4e612c0b27cc3c4861ff81299e09dea6d63015
cclutta <chelsea.lutta@gmail.com>
2022-09-26 07:42:44 +0300

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user
+    Args:
+        url: the url to fetch
+        q: search term
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = "" if sys.argv[1] is None else sys.argv[1]
+
+    values = {"q": q}
+    res = requests.post(url, data=values)
+    try:
+        json = res.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
