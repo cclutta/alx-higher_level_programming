1dd1afec29e53e206174df37ae43167a556f8119
cclutta <chelsea.lutta@gmail.com>
2022-09-26 08:21:51 +0300

    Update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     owner = sys.argv[2]
 
     url = "https://api.github.com/repos/"+owner+"/"+repo+"/commits?per_page=10"
-    
+
     res = requests.get(url)
     commits = res.json()
-    
+
     for commit in commits:
         print("{}: {}".format(commit.get("sha"),
                               commit.get("commit").get("author").get("name")))
