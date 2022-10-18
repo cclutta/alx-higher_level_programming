faaa954c1139bba51f41eaebaeb29634b320535b
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-18 21:56:57 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,7 +4,6 @@ const req = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-
 req.get(url, (err, res, body) => {
   if (err) console.log(err);
   else {
