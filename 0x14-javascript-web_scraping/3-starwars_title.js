d274d9122e0127bfa8fab10e1af69bb5619a28f2
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-18 21:39:36 +0300

    Create 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+req.get(url, (err, res, body) => {
+  if (err) console.log(err);
+  else console.log(JSON.parse(body).title);
+});
