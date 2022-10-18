25fce89e64523da8c87e1b26f735b5576da0dfc3
cclutta <52520502+cclutta@users.noreply.github.com>
2022-10-18 21:54:33 +0300

    Create 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+
+req.get(url, (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    JSON.parse(body).characters.forEach(character => {
+      req.get(character, (err, res, body) => {
+        if (err) console.log(err);
+        else console.log(JSON.parse(body).name);
+      });
+    });
+  }
+});
